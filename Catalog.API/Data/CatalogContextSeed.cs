90f1c0bf2f0125fb6897bba0e7dece3f8675d919
ThiagoMax <thiagomaximiliano21@gmail.com>
2022-08-09 14:01:19 -0300

    Creating the Product entity and context

---
+++ Catalog.API/Data/CatalogContextSeed.cs
@@ -0,0 +1,29 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContextSeed
+    {
+        public static void SeedData(IMongoCollection<Product> productCollection)
+        {
+            bool existProduct = productCollection.Find(p => true).Any();
+            if (!existProduct)
+            {
+                productCollection.InsertManyAsync(GetMyProducts());
+            }
+        }
+
+        private static IEnumerable<Product> GetMyProducts()
+        {
+            return new List<Product>()
+            {
+                Id = "" // parei akii.. popular a lista de produtos com dados aleatórios
+            };
+        }
+    }
+}
